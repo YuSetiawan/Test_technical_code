45e1cb9a891bfd366b6646fd120c841a74beb109
YuSetiawan <ysfstiawan@gmail.com>
2023-10-18 15:12:08 +0700

    deleted redux configuration

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import {Provider} from 'react-redux';
-import store from './config/redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);
